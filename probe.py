3c4286532e5fd29ed6977f5abffbd5f1479258bb
dhruvji <dhruvgautam@berkeley.edu>
2024-04-10 10:22:17 +0000

    probe fi

--- probe.py
+++ probe.py
@@ -1,40 +1,28 @@
 import torch
 import torch.nn as nn
 import torch.optim as optim
-from torch.utils.data import DataLoader, TensorDataset
-import numpy as np
 import pickle
 
-pickle_file_path = '/data/dhruv_gautam/llava-internal/caches/reg/activation_cache2_100205.pkl'
+pickle_file_path_normal = '/data/dhruv_gautam/llava-internal/caches/reg/activation_cache2_100205.pkl'
+pickle_file_path_red = '/data/dhruv_gautam/llava-internal/caches/red/activation_cache2_100205.pkl'
 
-with open(pickle_file_path, 'rb') as file:
-    activation_cache = pickle.load(file)
-
-pickle_file_path_red = '/data/dhruv_gautam/llava-internal/caches/reg/activation_cache2_100205.pkl'
+with open(pickle_file_path_normal, 'rb') as file:
+    activation_cache_normal = pickle.load(file)
 
 with open(pickle_file_path_red, 'rb') as file:
     activation_cache_red = pickle.load(file)
 
-layer_index = x  
-activations_normal = activation_cache[layer_index].view(activation_cache[layer_index].size(0), -1)
-activations_red = activation_cache_red[layer_index].view(activation_cache_red[layer_index].size(0), -1)
-
-labels = torch.cat((torch.zeros(activations_normal.size(0)), torch.ones(activations_red.size(0))), dim=0)
-
-activations = torch.cat((activations_normal, activations_red), dim=0)
-
-dataset = TensorDataset(activations, labels)
-data_loader = DataLoader(dataset, batch_size=16, shuffle=True)
+layer_index = 30
 
 class BinaryProbe(nn.Module):
     def __init__(self, num_features):
         super(BinaryProbe, self).__init__()
-        self.linear = nn.Linear(num_features, 1)  
+        self.linear = nn.Linear(num_features, 1)
 
     def forward(self, x):
         return self.linear(x)
 
-num_features = activations.shape[1] 
+num_features = activation_cache_normal[layer_index][0].numel()
 probe = BinaryProbe(num_features)
 
 criterion = nn.BCEWithLogitsLoss()
@@ -42,14 +30,25 @@ optimizer = optim.Adam(probe.parameters(), lr=0.001)
 
 num_epochs = 10
 for epoch in range(num_epochs):
-    for inputs, targets in data_loader:
-        optimizer.zero_grad()
-
-        outputs = probe(inputs).squeeze(1)  
-        loss = criterion(outputs, targets)
+    total_loss = 0
+    optimizer.zero_grad()  
+    for instance in activation_cache_normal[layer_index]:
+        instance_flat = instance.view(-1).unsqueeze(0)  
+        label = torch.tensor([0.0])  
+        output = probe(instance_flat).squeeze()  
+        loss = criterion(output, label)
+        loss.backward()  
+        total_loss += loss.item()
+    for instance in activation_cache_red[layer_index]:
+        instance_flat = instance.view(-1).unsqueeze(0)
+        label = torch.tensor([1.0])  
+        output = probe(instance_flat).squeeze()
+        loss = criterion(output, label)
         loss.backward()
-        optimizer.step()
+        total_loss += loss.item()
 
-    print(f'Epoch {epoch+1}, Loss: {loss.item()}')
+    optimizer.step()
+    avg_loss = total_loss / (len(activation_cache_normal[layer_index]) + len(activation_cache_red[layer_index]))
+    print(f'Epoch {epoch + 1}, Average Loss: {avg_loss}')
 
 print('Finished Training Probe')
